106b517396a0ec1424dc934f0688b8fe7d261e5e
mukesh kumar <mukeshkumar130803@gmail.com>
2023-08-09 17:21:14 +0530

    name change

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const http=require("http");
 
 const server=http.createServer((req,res)=>{
     if(req.url=="/"){
-        res.end("<h1>hi, this is mukesh from home page</h1>");
+        res.end("<h1>hi, this is <b>jothlingam</b> from home page</h1>");
     }
     else if(req.url=="/about"){
-        res.end("<h1>hi, this is mukesh from about page</h1>");
+        res.end("<h1>hi, this is <b>jothilingam</b> from about page</h1>");
     }
     else{
         res.end("<h1>sorry i didn't get you!!</h1>");
